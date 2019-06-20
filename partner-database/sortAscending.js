b70241ed8e2e39c1254f5a4303a67111f83d4897
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-06-20 16:30:01 +0200

    Add sortAscending

---
+++ partner-database/sortAscending.js
@@ -0,0 +1,23 @@
+function onEditASS(event){
+    //var sheet = event.source.getActiveSheet();
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Assessments") 
+    var editedCell = sheet.getActiveCell();
+    var columnToSortBy = 1;
+    var tableRange = "A2:AK";
+    var message = 'The list has been sorted ascending - check for your entry.';
+    var title = 'Heads-up!';
+  
+    if(editedCell.getColumn() == columnToSortBy){   
+        var range = sheet.getRange(tableRange);
+        
+        range.sort({
+            column : columnToSortBy,
+            ascending: true
+        });
+        
+        SpreadsheetApp
+            .getActiveSpreadsheet()
+            .toast(message, title, 4);
+    }
+}
+  
