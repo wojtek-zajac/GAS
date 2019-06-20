6d5b045472af135d22280f3b0fcd0ae0892002bc
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-06-20 16:45:01 +0200

    Add setTimestamp

---
+++ partner-database/setTimestampForLiveItem.js
@@ -0,0 +1,19 @@
+function markRowLive() {
+    var s = SpreadsheetApp.getActiveSheet();
+    var sheetName = s.getSheetName();
+    var r = s.getActiveCell();
+    var cellContent = r.getValue();
+    
+    if(sheetName === "Assessments" && r.getColumn() === 2 && cellContent === "Live") { 
+        var row = r.getRow();
+        var time = new Date();
+        
+        // time = Utilities.formatDate(time, "GMT+01:00", "MM/dd/yy HH:mm:ss");   
+        time = Utilities.formatDate(time, "GMT+01:00", "MM/dd/yy"); 
+   
+        SpreadsheetApp
+            .getActiveSheet()
+            .getRange('G' + row.toString())
+            .setValue(time); 
+    }
+}
