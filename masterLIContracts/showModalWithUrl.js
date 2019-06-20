997828792afb664b23ad6c2f1858470edd27e509
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-06-20 22:20:17 +0200

    Add showModalWithUrl

---
+++ masterLIContracts/showModalWithUrl.js
@@ -0,0 +1,19 @@
+function onW() {
+    var ui = SpreadsheetApp.getUi();
+    ui.createMenu('Custom Menu')
+        .addItem('First item', 'menuItem1')
+        .addToUi();
+}
+
+function menuItem1(){
+    showURL("https://www.youtube.com/watch?v=Wfzp4cdcuYc");
+}
+
+function showURL(href){
+    var app = UiApp.createApplication().setHeight(50).setWidth(200);
+    var link = app.createAnchor('https://www.youtube.com/watch?v=Wfzp4cdcuYc', href).setId("link");
+    var doc = SpreadsheetApp.getActive();
+    app.setTitle("Open Link");
+    app.add(link);  
+    doc.show(app);
+}
