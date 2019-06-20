2a46c050e60b1d1738c1fa6f4873a2b341ae249d
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-06-20 22:23:04 +0200

    Add onEditIndia

--- masterLIContracts/showModalWithUrl.js
+++ masterLIContracts/showModalWithUrl.js
@@ -9,6 +9,13 @@ function menuItem1(){
     showURL("https://www.youtube.com/watch?v=Wfzp4cdcuYc");
 }
 
+function onEditIndia() {  
+    var s = SpreadsheetApp.getActiveSheet();
+    if (s.getName() == 'IN') {
+        showURL("https://www.youtube.com/watch?v=Wfzp4cdcuYc");
+    }
+}
+
 function showURL(href){
     var app = UiApp.createApplication().setHeight(50).setWidth(200);
     var link = app.createAnchor('https://www.youtube.com/watch?v=Wfzp4cdcuYc', href).setId("link");
