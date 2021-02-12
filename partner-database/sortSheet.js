0da3c85cac67d67908eab3308f0de4bb0d4cd464
Wojciech Zając <wojciechzajaaac@gmail.com>
2021-02-12 18:16:46 +0100

    Update sortSheet.js

--- partner-database/sortSheet.js
+++ partner-database/sortSheet.js
@@ -1,11 +1,10 @@
-function sortDocumentationData(event){
-    //var sheet = event.source.getActiveSheet();
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Documentation-Data") 
+function sortDocumentationData(event) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Documentation-Data");
     var editedCell = sheet.getActiveCell();
     var columnToSortBy = 1;
     var tableRange = "A2:D";
   
-    if(editedCell.getColumn() == columnToSortBy){   
+    if (editedCell.getColumn() == columnToSortBy) {   
         var range = sheet.getRange(tableRange);
     
         range.sort({
