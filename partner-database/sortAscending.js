408334769e9f08f0693ad1f384722359900ab2af
Wojciech Zając <wojciechzajaaac@gmail.com>
2021-02-12 18:16:15 +0100

    Update sortAscending.js

--- partner-database/sortAscending.js
+++ partner-database/sortAscending.js
@@ -1,13 +1,12 @@
-function onEditASS(event){
-    //var sheet = event.source.getActiveSheet();
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Assessments") 
+function onEditASS(event) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Assessments");
     var editedCell = sheet.getActiveCell();
     var columnToSortBy = 1;
     var tableRange = "A2:AK";
     var message = 'The list has been sorted ascending - check for your entry.';
     var title = 'Heads-up!';
   
-    if(editedCell.getColumn() == columnToSortBy){   
+    if (editedCell.getColumn() == columnToSortBy) {   
         var range = sheet.getRange(tableRange);
         
         range.sort({
