681bc3980ff1430025bafb7409354aa3420c367c
Wojciech Zając <wojciechzajaaac@gmail.com>
2021-02-12 18:12:00 +0100

    Update getEmails.js

--- partner-database/getEmails.js
+++ partner-database/getEmails.js
@@ -1,17 +1,17 @@
 function gmail_sheet() {
-    var ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("New Marketplace Requests") 
+    var ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("New Marketplace Requests");
     var label = GmailApp.getUserLabelByName("New Mrkpl Req");
     var threads = label.getThreads();
       
-    for (var i=0; i<threads.length; i++){
+    for (var i=0; i<threads.length; i++) {
     var messages = threads[i].getMessages();
         
-        for (var j=0; j<messages.length; j++){
+        for (var j=0; j<messages.length; j++) {
             var msg = messages[j].getPlainBody();
             var sub = messages[j].getSubject();
             var dat = messages[j].getDate();
         
-            ss.appendRow([msg, sub, dat])           
+            ss.appendRow([msg, sub, dat]);       
         }
 
         threads[i].removeLabel(label);  
